e31a699a0a29a45497e911ae23cc37421c396543
kinuko1221 <kinuko1221@net44-dhcp205.sfc.keio.ac.jp>
2016-06-09 10:32:07 +0900

    第04問(完)

--- 04/04.js
+++ 04/04.js
@@ -1,6 +1,5 @@
 function xor(fact1, fact2){
-   var result;
-result = !fact1 || fact2;
- return !result;
-  
+var result;
+    result = (fact1 && !fact2) || (!fact1 && fact2);
+    return result;
 }
